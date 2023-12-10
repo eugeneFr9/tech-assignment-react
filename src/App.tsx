4ec72c3482a1d25781fc8984b2b90fb2f9a3e6f3
Evgenii <evgenii.frank@lunatech.nl>
2023-12-10 15:39:28 +0100

    assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import JsonViewer from "./JsonViewer";
 
-function App() {
+const jsonResponse = `{
+  "resp": {
+    "date": "2021-10-27T07:49:14.896Z",
+    "hasError": false,
+    "fields": [
+      {
+        "id": "4c212130",
+        "prop": "iban",
+        "value": "DE81200505501265402568",
+        "hasError": false
+      }
+    ]
+  }
+}`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <JsonViewer response={JSON.parse(jsonResponse)} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/JsonViewer.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import "./JsonViewer.css";
+
+interface Response {
+  resp: FetchedData;
+}
+
+interface Field {
+  id: string;
+  prop: string;
+  value: string;
+  hasError: boolean;
+}
+
+interface FetchedData {
+  date: string;
+  hasError: boolean;
+  fields: Field[];
+}
+
+const JsonViewer: React.FC<{ response: Response }> = ({ response }) => {
+  const [selectedKey, setSelectedKey] = useState<string>("");
+
+  const handleSelectKey = (key: string) => {
+    setSelectedKey(key);
+  };
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setSelectedKey(event.target.value);
+
+  const retrieveValue = (selectedPath: string): any => {
+    if (
+      (selectedKey.includes("[") && !selectedKey.includes("]")) ||
+      selectedKey.endsWith(".")
+    ) {
+      return "undefined";
+    }
+    const keys = selectedPath
+      .split(/\.|\[|\]/)
+      .filter((part) => part.trim() !== "")
+      .map((part) => (!isNaN(Number(part)) ? parseInt(part, 10) : part));
+
+    const result = keys.reduce((acc: any, key: number | string) => {
+      if (acc && typeof acc === "object" && key in acc) {
+        return acc[key];
+      } else {
+        return "undefined";
+      }
+    }, response);
+
+    return typeof result === "object" ? "" : result;
+  };
+
+  const renderValue = (
+    value: any,
+    parentKeys: string = ""
+  ): React.ReactNode => {
+    if (typeof value === "object" && value !== null) {
+      return Array.isArray(value) ? (
+        <>
+          {"["}
+          {value.map((item, index) => (
+            <div key={index} style={{ marginLeft: "20px" }}>
+              {renderValue(item, `${parentKeys}[${index}]`)}
+            </div>
+          ))}
+          {"]"}
+        </>
+      ) : (
+        <>
+          {"{"}
+          {Object.entries(value).map(
+            ([key, innerValue], innerIndex, innerArray) => (
+              <div key={key}>
+                <span
+                  className="json-key"
+                  onClick={() => handleSelectKey(`${parentKeys}.${key}`)}
+                >{`${key}: `}</span>
+                {renderValue(innerValue, parentKeys)}
+                {innerIndex < innerArray.length - 1 ? "," : ""}
+              </div>
+            )
+          )}
+          {"}"}
+        </>
+      );
+    } else {
+      return (
+        <span>
+          {typeof value === "string" ? `'${value}'` : JSON.stringify(value)}
+        </span>
+      );
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="input-wrapper">
+        <h2>Property</h2>
+        <input
+          type="text"
+          value={selectedKey}
+          onChange={(event) => onChangeHandler(event)}
+        />
+        <span className="result-value">
+          {selectedKey && retrieveValue(selectedKey).toString()}
+        </span>
+      </div>
+      <h2>Response</h2>
+      {Object.keys(response).map((mainKey) => (
+        <div key={mainKey} className="json-data">
+          {Object.entries(response.resp).map(([key, value], index, array) => (
+            <div key={key}>
+              <span
+                className={`json-key ${
+                  Array.isArray(value) ? "exclude-key" : ""
+                }`}
+                onClick={() => {
+                  if (!Array.isArray(value)) {
+                    handleSelectKey(`${mainKey}.${key}`);
+                  }
+                }}
+              >{`${key}: `}</span>
+              {renderValue(value, `${mainKey}.${key}`)}
+              {index < array.length - 1 ? "," : ""}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default JsonViewer;
